219f61a13fad9804dc15149fdfc385537a7ae2c0
Vidyashree <vidyashreeks80@gmail.com>
2022-05-06 00:21:58 +0530

    second

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Helcome to Spotify");
 let songIndex=0;
 let audioElement=new Audio('1.mp3');
 let masterPlay=document.getElementById('masterPlay');
-let myProgressBar=document.getElenentById('myProgressBar');
+let myProgressBar=document.getElememtById('myProgressBar');
 let songs=[
     {songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Salan-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
@@ -12,3 +12,15 @@ let songs=[
     {songName: "Salan-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Salan-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
 ]
+
+masterPlay.addEventListener("click", ()=> {
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+    }
+    
+})
+
+myProgressBar.addEventlistener('timeupdate', ()=>{
+    console.log('timeupdate');
+   // Update Seekbar
+})
